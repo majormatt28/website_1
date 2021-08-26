8e9fd24e6f690b209fda5abb3ebffbf385469d73
Matthew <elijah2895@hotmail.com>
2021-08-25 22:38:12 -0400

    weworded files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react';
+import Blogs from './Blogs';
+import AboutMe from './AboutMe';
+import MainPage from './MainPage'
 
 function App() {
   return (
     <div className="App">
-     
+      <Blogs />
+      <AboutMe />
+      <MainPage />
     </div>
   );
 }
