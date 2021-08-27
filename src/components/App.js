411a102e0ffca7850d1a6a98509472565326338e
Matthew <elijah2895@hotmail.com>
2021-08-27 15:11:59 -0400

    moved around a few things

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import Blogs from './Blogs';
 import AboutMe from './AboutMe';
 import Header from './Header'
 
-import projects from "../data/projects";
-
 function App() {
   return (
     <div className="App">
